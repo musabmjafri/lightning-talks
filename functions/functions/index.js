826560b5533b4e4aabe7ff678cccf8a9a5cc6e42
Musab M. Jafri <33220062+musabmjafri10p@users.noreply.github.com>
2019-02-06 12:16:29 +0500

    Cloud functions base structure commit

---
+++ functions/functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+//var serviceAccount = require('path/to/serviceAccountKey.json');
+//admin.initializeApp({
+//  credential: admin.credential.cert(serviceAccount),
+//  databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+//});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
